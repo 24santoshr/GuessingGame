8a1a4c43d2a11b8d4c59f57717aec18d54168523
santosh <santosh.r@hanatectpvt.com>
2023-09-28 15:17:14 +0530

    commenting out unit test cases

--- GuessingGameTest/UnitTest1.cs
+++ GuessingGameTest/UnitTest1.cs
@@ -1,19 +1,19 @@
-namespace GuessingGameTest;
+// namespace GuessingGameTest;
 
-[TestClass]
-public class ServiceTest
-    {
-        [TestMethod]
-        public void generateOutput_WithVaildValues()
-        {
+// [TestClass]
+// public class ServiceTest
+//     {
+//         [TestMethod]
+//         public void generateOutput_WithVaildValues()
+//         {
 
-            int betAmount = 100;
-            int guessNumber = 4;
-            int difficulty = 3;
+//             int betAmount = 100;
+//             int guessNumber = 4;
+//             int difficulty = 3;
 
-            Service service = new Service();
-            result = service.generateOutput(100, 4, 3);
+//             Service service = new Service();
+//             result = service.generateOutput(100, 4, 3);
 
-            Assert.IsString(result);
-        }
-    }
+//             Assert.IsString(result);
+//         }
+//     }
